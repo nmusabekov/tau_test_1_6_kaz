461950156f2fe105d154c8f4ae4331c5f27dc52c
nmusabekov <n.musabekov01@gmail.com>
2026-03-06 04:08:02 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // ======= TAU RK1 CONFIG =======
-const SUBMIT_URL = "https://script.google.com/macros/s/AKfycbx6CxLPk45py6Owp8wKDZUXjr2nuNw5WUu2stf3eKQDkWZMfss4xmcmguPLgyTvNo2wVQ/exec";
+const SUBMIT_URL = "https://script.google.com/macros/s/AKfycbxInNJ3b0bk_4YEzZeSYTzvTtOtvkKqCDQln5Zr8bVWghn170ApPQ348owVxjqHx_4gtg/exec";
 
 // Пән/тест атауы
 const QUIZ_TITLE = "TAU 22211-ТАУ Тест 1 (Дәріс 1–6)";
